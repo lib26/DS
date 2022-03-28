e293e0aefe139e3453c77ae4bfadf36428d7d773
lib26 <dladlsqja@naver.com>
2022-03-28 23:59:17 +0900

    추가 설명

--- HW2_Pandas.py
+++ HW2_Pandas.py
@@ -24,12 +24,12 @@ print(df.isna().sum())
 print()
 
 # dropna with howany, howall, thresh1, thresh2
-print("---------dropna/axis=0---------")
+print("---------dropna/axis=0---------") #가로
 print(df.dropna(axis=0, how='all'))
 print(df.dropna(axis=0, how='any'))
 print(df.dropna(axis=0, how='any', thresh=1))
 print(df.dropna(axis=0, how='any', thresh=2))
-print("---------dropna/axis=1---------")
+print("---------dropna/axis=1---------") #세로
 print(df.dropna(axis=1, how='all'))
 print(df.dropna(axis=1, how='any'))
 print(df.dropna(axis=1, how='any', thresh=1))
@@ -45,6 +45,8 @@ print()
 print()
 
 # ffill, bfill
+# 여기 axis가 좀 헷갈릴 수 있는데
+# axis = 0 이라면, '가로 안에서' 보단 '가로에 대하여' 라고 이해하기
 print("---------fillna(ffill/bfill)---------")
 print(df.fillna(axis=0, method='ffill'))
 print(df.fillna(axis=0, method='bfill'))
